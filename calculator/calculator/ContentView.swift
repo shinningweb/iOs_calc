fc94f786231226743f1e6028affe4f7527ea1ccf
Ebenezer Nii Otoo <askeben@loveworld360.com>
2020-01-02 11:17:04 +0000

    single page app template

---
+++ calculator/calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by GROW on 02/01/2020.
+//  Copyright © 2020 GROW. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
